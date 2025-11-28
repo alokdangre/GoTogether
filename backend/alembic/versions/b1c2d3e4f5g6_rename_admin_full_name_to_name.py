dc1d2767a9c642bd67dda3b4eae070ab9a50ff84
GoTogether Developer <alokdangre@gmail.com>
2025-11-28 19:10:44 +0000

    corrected the admin migrations again

---
+++ backend/alembic/versions/b1c2d3e4f5g6_rename_admin_full_name_to_name.py
@@ -0,0 +1,24 @@
+"""rename admin full_name to name
+
+Revision ID: b1c2d3e4f5g6
+Revises: aee12e345291
+Create Date: 2025-11-28 19:15:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b1c2d3e4f5g6'
+down_revision = 'aee12e345291'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.alter_column('admins', 'full_name', new_column_name='name')
+
+
+def downgrade() -> None:
+    op.alter_column('admins', 'name', new_column_name='full_name')
