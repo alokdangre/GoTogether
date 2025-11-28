33e81999d5a14b61d394b6c5cbda075070aa9f55
GoTogether Developer <alokdangre@gmail.com>
2025-11-28 19:13:29 +0000

    fixed

--- backend/alembic/versions/b1c2d3e4f5g6_rename_admin_full_name_to_name.py
+++ backend/alembic/versions/b1c2d3e4f5g6_rename_admin_full_name_to_name.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'b1c2d3e4f5g6'
-down_revision = 'aee12e345291'
+down_revision = '9b828df5dbe9'
 branch_labels = None
 depends_on = None
 
