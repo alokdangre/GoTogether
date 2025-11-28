b97246d0b1e91e467e4176231c0014572b432a78
GoTogether Developer <alokdangre@gmail.com>
2025-11-28 19:18:38 +0000

    fixed again

---
+++ backend/alembic/versions/c2d3e4f5g6h7_add_is_active_to_admins.py
@@ -0,0 +1,24 @@
+"""add is_active to admins
+
+Revision ID: c2d3e4f5g6h7
+Revises: b1c2d3e4f5g6
+Create Date: 2025-11-28 19:20:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c2d3e4f5g6h7'
+down_revision = 'b1c2d3e4f5g6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('admins', sa.Column('is_active', sa.Boolean(), server_default='true', nullable=False))
+
+
+def downgrade() -> None:
+    op.drop_column('admins', 'is_active')
